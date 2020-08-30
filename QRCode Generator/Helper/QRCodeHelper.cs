7a8b42d5633bdfe4d63bf363845edd0262c54d56
Richard <Richard@DESKTOP-9LM3OJE>
2020-08-30 17:04:52 -0300

    Finalizando.

---
+++ QRCode Generator/Helper/QRCodeHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+
+namespace QRCode_Generator.Helper
+{
+    public static class QRCodeHelper
+    {
+        public static string TextToQrCode(string texto)
+        {
+            var writer = new ZXing.BarcodeWriterPixelData
+            {
+                Format = ZXing.BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Width = 180,
+                    Height = 180,
+                    Margin = 0,
+                    QrVersion = 10,
+                    ErrorCorrection = ErrorCorrectionLevel.L
+                }
+            };
+            var pixelData = writer.Write(texto);
+            using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb))
+            {
+                using (var ms = new System.IO.MemoryStream())
+                {
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+                    try
+                    {
+                        System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                    }
+                    finally
+                    {
+                        bitmap.UnlockBits(bitmapData);
+                    }
+
+                    bitmap.Save(ms, ImageFormat.Png);
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+    }
+}
--- QRCode Generator/Pages/Index.cshtml.cs
+++ QRCode Generator/Pages/Index.cshtml.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace QRCode_Generator.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public string Texto { get; set; }
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPost(string texto)
+        {
+            TempData["Image"] = Helper.QRCodeHelper.TextToQrCode(texto);
+            return RedirectToPage("Index");
         }
     }
 }
